2a4810a653166e2f0002b5d626fe49fb2a55e16a
Arnstein Johansen <arnstej@gmail.com>
2013-09-15 11:07:45 +0200

    facebooklogin

--- DateIt/www/js/app.js
+++ DateIt/www/js/app.js
@@ -1,11 +1,40 @@
-'use strict';
+﻿/// <reference path='_all.ts' />
+var dateIt;
+(function (dateIt) {
+    'use strict';
 
+    var services = angular.module("dateIt.Services", []);
+    services.service("datacontext", dateIt.DatacontextService.prototype.injection());
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-      $routeProvider.when('/infridge', { templateUrl: '/partials/inFridge.html', controller: InFridgeController });
-      $routeProvider.when('/addByText', { templateUrl: 'partials/addByText.html', controller: AddItemController });
-      $routeProvider.otherwise({ redirectTo: '/view1' });
-      //$locationProvider.html5Mode(true).hashPrefix('!');
-  }]);
+    var controllers = angular.module("dateIt.Controllers", []);
+    controllers.controller("fridgeCtrl", dateIt.FridgeController.prototype.injection());
+    controllers.controller("loginCtrl", dateIt.LoginController.prototype.injection());
+    controllers.controller("addItemCtrl", dateIt.AddItemController.prototype.injection());
+
+    var app = angular.module("dateIt", ["Mac", "Scope.safeApply", "dateIt.Controllers", "dateIt.Services"]);
+
+    //.service("datacontext", dateIt.DatacontextService.prototype.injection());;
+    // Declare app level module which depends on filters, and services
+    app.config([
+        '$routeProvider',
+        '$locationProvider',
+        function ($routeProvider, $locationProvider) {
+            $routeProvider.when('/infridge', {
+                templateUrl: '/partials/inFridge.html',
+                controller: "fridgeCtrl"
+            });
+            $routeProvider.when('/addByText', {
+                templateUrl: 'partials/addByText.html',
+                controller: "addItemCtrl"
+            });
+            $routeProvider.when('/login', {
+                templateUrl: 'partials/login.html',
+                controller: "loginCtrl"
+            });
+
+            $routeProvider.otherwise({ redirectTo: '/login' });
+            //$locationProvider.html5Mode(true).hashPrefix('!');
+        }
+    ]);
+})(dateIt || (dateIt = {}));
+//# sourceMappingURL=app.js.map
